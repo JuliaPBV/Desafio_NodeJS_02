3f246d2edb76f1463391fba8c0e5959d17629aa2
JuliaPBV <vital.pereira.julia@gmail.com>
2024-03-28 13:43:35 -0300

    Imprimir na tela os seguintes requerimentos: R01 – SOMAR A IDADE DE TODOS OS UBUNTUS
    R02 – AGRUPAR O NOME DE TODOS OS UBUNTUS
    R03 – CALCULAR MÉDIA IMC DE TODOS UBUNTOS
    R04 – CONTAR QUANTOS UBUNTUS SÃO DEVS
    R05 – EXIBIR SOMENTE UBUNTUS COM SOBRENOME SILVA

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,49 @@ app.listen(PORT, () => {
 
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
+	// R01, R02, R03, R04, R05	
+	const nome1 = 'Ariane Silva';
+	const idade1 = 28;
+	const peso1 = 60;
+	const altura1 = 1.60;
+	const dev1 = true;
+	let IMC1
 	
-});
+	const nome2 = 'Henrique OLiveira da Silva';
+	const idade2 = 35;
+	const peso2 = 85;
+	const altura2 = 1.80;
+	const dev2 = true;
+	let IMC2
+	
+	const nome3 = 'Emille Silva';
+	const idade3 = 25;
+	const peso3 = 75;
+	const altura3 = 1.70;
+	const dev3 = true;
+	let IMC3 
+	
+	const nome4 = 'Karina Alves';
+	const idade4 = 27;
+	const peso4 = 70;
+	const altura4 = 1.70;
+	const dev4 = true;
+    let IMC4 
+	
+	const somaDasIdades = idade1 + idade2 + idade3 + idade4;
+	console.log('Soma das idades dos Ubuntus:', somaDasIdades);
+
+    console.log(`${nome1}, ${nome2}, ${nome3}, ${nome4}.`);
+
+	
+    const mediaIMC = (IMC1 + IMC2 + IMC3 + IMC4) / 4;
+    console.log('Média do IMC de todos os Ubuntus:', mediaIMC);
+	
+	const devs = [dev1, dev2, dev3, dev4];
+	const totalDevs = devs.filter(dev => dev).length;
+	console.log('Total de Ubuntos que são devs:', totalDevs);
+	
+	const UbuntusSilva = [nome1, nome2, nome3, nome4];
+	const UbuntusComSilva = UbuntusSilva.filter(nome => nome.includes('Silva'));
+	console.log('Ubuntus com sobrenome Silva:', UbuntusComSilva);
+	});
